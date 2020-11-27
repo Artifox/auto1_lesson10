e77f491517c8e86449a4c082c3cec4950b4e97db
AKoptsiukh <sid_93@mail.ru>
2020-11-27 22:11:31 +0300

    Task1

--- src/lesson1/Lesson1HomeWork.java
+++ src/lesson1/Lesson1HomeWork.java
@@ -3,5 +3,18 @@ package lesson1;
 public class Lesson1HomeWork {
     public static void main(String[] args) {
         System.out.println("Lesson1 HomeWork");
+        System.out.println(task1(0));
+    }
+
+
+    public static int task1(int number){
+        if(number > 0) {
+            number++;
+        } else if(number < 0){
+            number-=2;
+        } else {
+            number = 10;
+        }
+        return number;
     }
 }
