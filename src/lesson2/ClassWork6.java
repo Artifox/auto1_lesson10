72c11144587fd8efa1ff2944dbb0ea7a4492bb9c
AKoptsiukh <sid_93@mail.ru>
2020-12-03 17:15:26 +0300

    Lesson2 ClassWork6 fix + Home Discovery

--- src/lesson2/ClassWork6.java
+++ src/lesson2/ClassWork6.java
@@ -9,9 +9,9 @@ public class ClassWork6 {
             System.out.println();
         }
 
-        for (int i = 4; i > 0; i--) {
-            for (int j = 1; j < i; j++) {
-                System.out.println("*");
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
